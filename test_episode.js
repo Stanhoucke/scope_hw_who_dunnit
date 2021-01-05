cce3551798eecc49cfda78f4eb1e113e2cec82aa
Stanley Houcke <stanhoucke@gmail.com>
2021-01-05 18:51:31 +0000

    Went through all episodes

---
+++ test_episode.js
@@ -0,0 +1,197 @@
+// Episode 1
+// Prediction - Miss Scarlet
+// Object declared using const, outside of function. Therefore, accessible inside function
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Episode 2
+// Prediction - Professor Plum. Returns error for trying to assign to a const variable.
+// Murderer variable defined with const. Const assignment cannot be changed, therefore murderer remains as Professor Plum
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Episode 3
+// Prediction - first verdict: Mrs. Peacock, second verdict: Professor Plum.
+// First verdict - declareMurderer function assigns Mrs. Peacock to let defined variable inside the function.
+// Second verdict - secondVerdict refers to initial variable declaration outside function.
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// Episode 4
+// Prediction - Colonel Mustard in suspects, but suspectThree is Mrs. Peacock.
+// declareAllSuspects re-assigns suspectThree to Colonel Mustard in the function.
+// suspectThree remains Mrs. Peacock in the variable outside the function.
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// Episode 5
+// Prediction - Revolver
+// Const variables cannot be re-assigned. However, contents of const variables can. Therefore, changeWeapon successfully changes the weapon to revolver.
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+
+// Episode 6
+// Prediction - Mrs. White
+// Call to plotTwist when changeMurderer is called. Murderer re-assigned by plotTwist.
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Episode 7
+// Prediction - Miss Scarlet. Outcome - Mr. Green
+/* let defined variable in plotTwist is changed by unexpectedOutcome.
+However, it does not re-assign murderer in changeMurderer.
+changeMurderer changes initial let defined murderer outside function to Mr. Green */
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Episode 8
+// Prediction - Candle stick
+/* Functions make legal changes to contents of const defined object.
+Function calls go through if conditions in plotTwist and unexpectedOutcome. */
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+// Episode 9
+// Prediction - Professor Plum
+/* let murderer assignment to Mrs. Peacock confined to if statemnet code block.
+Therefore, declareMurderer calls initial murderer definition which stores 'Professor Plum'. */
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
